ca20de0d47150825666bc1fbb65f0f4d96af189f
Jacob Vilevac <jakevilevac@gmail.com>
2023-11-06 05:22:49 -0800

    added 2/3rd navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
+import BookLogo from './assets/book-solid.svg?react';
+import SearchGlass from './assets/magnifying-glass-solid.svg?react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
     </>
   )
+}
+
+function NavBar() {
+    return (
+        <nav id='navbar-container'>
+            <a href='/' className='nav-element' id='nav-logo'>
+                <BookLogo style={{ height: '24px', fill: 'currentColor' }} alt="Home Book Icon" />
+                <p>MC Quest Book</p>
+            </a>
+            <li id='search-element'>
+                <input placeholder='Search' name='search-bar'></input>
+                <SearchGlass style={{ height: '20px', fill: 'currentColor'}} alt="Magnifying Glass representing Search" />
+            </li>
+        </nav>
+    )
 }
 
 export default App
